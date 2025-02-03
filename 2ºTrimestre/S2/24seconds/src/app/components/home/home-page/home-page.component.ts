42e83ac706304af4f978145eb9df310673d5f663
SebassG05 <152302090+SebassG05@users.noreply.github.com>
2025-02-03 18:17:03 +0100

    S2 Encuesta

--- 2ºTrimestre/S2/24seconds/src/app/components/home/home-page/home-page.component.ts
+++ 2ºTrimestre/S2/24seconds/src/app/components/home/home-page/home-page.component.ts
@@ -3,16 +3,27 @@ import { NavbarComponent } from '../../layout/navbar/navbar.component';
 import { FooterComponent } from '../../layout/footer/footer.component';
 import { HeroSectionComponent } from '../hero-section/hero-section.component';
 import { ProductFilterComponent } from '../product-filter/product-filter.component';
+import { SurveyComponent } from '../../survey/survey.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [NavbarComponent, FooterComponent, HeroSectionComponent, ProductFilterComponent],
+  imports: [NavbarComponent, FooterComponent, HeroSectionComponent, ProductFilterComponent, SurveyComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  showSurvey = false;
+
   onFilterChange(filters: any) {
     console.log('Filtros aplicados:', filters);
     // Aquí puedes manejar los filtros y actualizar la vista según sea necesario
+  }
+
+  openSurvey() {
+    this.showSurvey = true;
+  }
+
+  closeSurvey() {
+    this.showSurvey = false;
   }
 }
